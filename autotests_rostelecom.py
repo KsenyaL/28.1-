a8ef59ae4e7d8f676903c321a00d53d99e8e7427
KsenyaL <102944826+KsenyaL@users.noreply.github.com>
2022-11-27 19:57:53 +0500

    Update autotests_rostelecom.py

--- autotests_rostelecom.py
+++ autotests_rostelecom.py
@@ -4,7 +4,7 @@ from pages.auth_page import AuthPage
 from pages.registration_page import RegPage
 
 
-# Тест-кейс N-01
+# Тест-кейс TK-001
 # Корректное отображение "Стандартной страницы авторизации"
 def test_start_page_is_correct(web_browser):
     page = AuthPage(web_browser)
@@ -18,7 +18,7 @@ def test_start_page_is_correct(web_browser):
     assert page.logo_lk.get_text() == "Личный кабинет"
 
 
-# Тест-кейс N-02 (Bugs-02)
+# Тест-кейс TK-002 (FB-1)
 # Проверка элементов в левом и правом блоке страницы
 @pytest.mark.xfail(reason="Расположение элементов на странице не соответсвует ожидаемым требованиям")
 def test_location_of_page_blocks(web_browser):
@@ -27,7 +27,7 @@ def test_location_of_page_blocks(web_browser):
     assert page.lk_form.find(timeout=1)
 
 
-# Тест-кейс N-03(Bugs-03)
+# Тест-кейс TK-003 (FB-2)
 # Проверка названия таб выбора "Номер"
 @pytest.mark.xfail(reason="Таб выбора 'Номер' не соответсвует ожидаемым требованиям")
 def test_phone_tab(web_browser):
@@ -35,7 +35,7 @@ def test_phone_tab(web_browser):
     assert page.phone_tab.get_text() == "Номер"
 
 
-# Тест-кейс N-04(Bugs-04)
+# Тест-кейс TK-004 (FB-3)
 # Проверка название кнопки "Продолжить" в форме "Регестрация"
 @pytest.mark.xfail(reason="Кнопка должна иметь текст 'Продолжить'")
 def test_registration_page_and_continue_button(web_browser):
@@ -51,165 +51,165 @@ def test_registration_page_and_continue_button(web_browser):
     assert reg_page.continue_button.get_text() == "Продолжить"
 
 
-# Тест-кейс N-06
+# Тест-кейс TK-005
 # Регистрация пользователя с пустым полем "Имя", появления текста с подсказкой об ошибке
 def test_registration_page_with_empty_name_field(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
     reg_page.name_field.send_keys('')
-    reg_page.last_name_field.send_keys("Фамилия")
-    reg_page.email_or_mobile_phone_field.send_keys("test123@mail.ru")
-    reg_page.password_field.send_keys("Qazwsx124")
-    reg_page.password_confirmation_field.send_keys("Qazwsx124")
+    reg_page.last_name_field.send_keys("Леснова")
+    reg_page.email_or_mobile_phone_field.send_keys("lesnova@yandex.ru")
+    reg_page.password_field.send_keys("Qwert1234")
+    reg_page.password_confirmation_field.send_keys("Qwert1234")
     reg_page.continue_button.click()
     reg_page.error_message_name.is_visible()
     assert reg_page.error_message_name.get_text() == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
 
 
-# Тест-кейс N-07
+# Тест-кейс ТК-006
 # Регистрация пользователя с некорректным значением в поле "Имя"(< 2 символов), появление текста с подскаской об ошибке
 def test_registration_with_an_incorrect_value_in_the_name_field(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys('А')
-    reg_page.last_name_field.send_keys("Фамилия")
-    reg_page.email_or_mobile_phone_field.send_keys("test123@mail.ru")
-    reg_page.password_field.send_keys("Qazwsx124")
-    reg_page.password_confirmation_field.send_keys("Qazwsx124")
+    reg_page.name_field.send_keys('К')
+    reg_page.last_name_field.send_keys("Леснова")
+    reg_page.email_or_mobile_phone_field.send_keys("lesnova@yandex.ru")
+    reg_page.password_field.send_keys("Qwert1234")
+    reg_page.password_confirmation_field.send_keys("Qwert1234")
     reg_page.continue_button.click()
     reg_page.error_message_name.is_visible()
     assert reg_page.error_message_name.get_text() == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
 
 
-# Тест-кейс N-08
+# Тест-кейс ТК-007
 # Регестрация пользователя с некорректным значением в поле "Фамилия"(>30 символов), появление текста с подскаской об ошибке
 def test_registration_with_an_incorrect_value_in_the_last_name_field(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Татьяна")
-    reg_page.last_name_field.send_keys("фывапролджавыываппвуячсмитьйукенгшщфывапролф")
-    reg_page.email_or_mobile_phone_field.send_keys("test123@mail.ru")
-    reg_page.password_field.send_keys("Qazwsx124")
-    reg_page.password_confirmation_field.send_keys("Qazwsx124")
+    reg_page.name_field.send_keys("Ксения")
+    reg_page.last_name_field.send_keys("ЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛ")
+    reg_page.email_or_mobile_phone_field.send_keys("lesnova@yandex.ru")
+    reg_page.password_field.send_keys("Qwert1234")
+    reg_page.password_confirmation_field.send_keys("Qwert1234")
     reg_page.continue_button.click()
     reg_page.error_message_name.is_visible()
     assert reg_page.error_message_last_name.get_text() == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
 
 
-# Тест-кейс N-09
-# Регистрация пользователя с уже зарегистрированным номером,отображается оповещающая форма
+# Тест-кейс ТК-008
+# Регистрация пользователя с уже зарегистрированным номером, отображается оповещающая форма
 def test_registration_of_an_already_registered_user(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Людмила")
-    reg_page.last_name_field.send_keys("Чижик")
-    reg_page.email_or_mobile_phone_field.send_keys("+79210916809")
-    reg_page.password_field.send_keys("Test12345678")
-    reg_page.password_confirmation_field.send_keys("Test12345678")
+    reg_page.name_field.send_keys("К")
+    reg_page.last_name_field.send_keys("ЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛ")
+    reg_page.email_or_mobile_phone_field.send_keys("+79999999999")
+    reg_page.password_field.send_keys("www")
+    reg_page.password_confirmation_field.send_keys("www")
     reg_page.continue_button.click()
     assert reg_page.notification_form.is_visible
 
 
-# Тест-кейс N-10(Bugs-10)
+# Тест-кейс ТК-009 (FB-4)
 # Проверка кнопки "х" - закрыть всплывающее окно оповещения
 @pytest.mark.xfail(reason="Должна быть кнопка закрыть 'х'")
 def test_notification_form(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Людмила")
-    reg_page.last_name_field.send_keys("Чижик")
-    reg_page.email_or_mobile_phone_field.send_keys("+79210916809")
-    reg_page.password_field.send_keys("Test12345678")
-    reg_page.password_confirmation_field.send_keys("Test12345678")
+    reg_page.name_field.send_keys("К")
+    reg_page.last_name_field.send_keys("ЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛ")
+    reg_page.email_or_mobile_phone_field.send_keys("+79999999999")
+    reg_page.password_field.send_keys("www")
+    reg_page.password_confirmation_field.send_keys("www")
     reg_page.continue_button.click()
     assert reg_page.login_button.get_text() == 'Войти'
     assert reg_page.recover_password_button.get_text() == 'Восстановить пароль'
     assert reg_page.close_button.get_text() == 'x'
 
 
-# Тест-кейс N-11
-# Некорректный пароль при регестрации пользователя(< 8 символов), появления текста с подсказкой об ошибке
+# Тест-кейс ТК-010
+# Некорректный пароль при регестрации пользователя (< 8 символов), появления текста с подсказкой об ошибке
 def test_incorrect_password_during_registration(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Раиса")
-    reg_page.last_name_field.send_keys("Безымянная")
-    reg_page.email_or_mobile_phone_field.send_keys("test123@mail.ru")
-    reg_page.password_field.send_keys("оп78")
-    reg_page.password_confirmation_field.send_keys("оп78")
+    reg_page.name_field.send_keys("Ксения")
+    reg_page.last_name_field.send_keys("Леснова")
+    reg_page.email_or_mobile_phone_field.send_keys("lesnova@yandex.ru")
+    reg_page.password_field.send_keys("www")
+    reg_page.password_confirmation_field.send_keys("www")
     reg_page.continue_button.click()
     assert reg_page.error_message_password.get_text() == "Длина пароля должна быть не менее 8 символов"
 
 
-# Тест-кейс N-12
+# Тест-кейс ТК-011
 # Вход по неправильному паролю в форме "Авторизация" уже зарегистрированного пользователя, надпись "Забыл пароль"
 # перекрашивается в оранжнвый цвет
 def test_authorization_of_a_user_with_an_invalid_password(web_browser):
     page = AuthPage(web_browser)
-    page.phone.send_keys('+79210916809')
-    page.password.send_keys("Test")
+    page.phone.send_keys('+79999999999')
+    page.password.send_keys("www")
     page.btn_login.click()
     assert page.message_invalid_username_or_password.get_text() == "Неверный логин или пароль"
     assert "rt-link--orange" in page.the_element_forgot_the_password.get_attribute('class')
 
 
-# Тест-кейс N-13
-# Регистрация пользователя в форме "Регистрации" в поле ввода "Фамилия" вместо кириллицы, недопустимые символы
+# Тест-кейс ТК-012
+# Регистрация пользователя в форме "Регистрации" в поле ввода "Фамилия" вместо кириллицы недопустимые символы
 def test_instead_of_cyrillic_invalid_characters(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Татьяна")
-    reg_page.last_name_field.send_keys("@@@@!!!")
-    reg_page.email_or_mobile_phone_field.send_keys("test123@mail.ru")
-    reg_page.password_field.send_keys("Qazwsx124")
-    reg_page.password_confirmation_field.send_keys("Qazwsx124")
+    reg_page.name_field.send_keys("Ксения")
+    reg_page.last_name_field.send_keys("%%%")
+    reg_page.email_or_mobile_phone_field.send_keys("lesnova@yandex.ru")
+    reg_page.password_field.send_keys("Qwert1234")
+    reg_page.password_confirmation_field.send_keys("Qwert1234")
     reg_page.continue_button.click()
     assert reg_page.message_must_be_filled_in_cyrillic.get_text() == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
 
 
-# Тест-кейс N-14
+# Тест-кейс ТК-013
 # Поле ввода "Пароль" и поле ввода "Подтверждение пароля"  в форме "Регистрация" не совпадают
 def test_password_and_password_confirmation_do_not_match(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Татьяна")
-    reg_page.last_name_field.send_keys("Фамилия")
-    reg_page.email_or_mobile_phone_field.send_keys("test123@mail.ru")
-    reg_page.password_field.send_keys("Test123456789")
-    reg_page.password_confirmation_field.send_keys("Test12345")
+    reg_page.name_field.send_keys("Ксения")
+    reg_page.last_name_field.send_keys("Леснова")
+    reg_page.email_or_mobile_phone_field.send_keys("lesnova@yandex.ru")
+    reg_page.password_field.send_keys("Qwert1234")
+    reg_page.password_confirmation_field.send_keys("www")
     reg_page.continue_button.click()
     assert reg_page.message_passwords_dont_match.get_text() == "Пароли не совпадают"
 
 
-# Тест-кейс N-15
+# Тест-кейс TK-014
 # Не валидный email в поле ввода "Email или мобильный телефон"
 def test_invalid_email_or_mobile_phone(web_browser):
     auth_page = AuthPage(web_browser)
     auth_page.registration_link.click()
     reg_page = RegPage(web_browser, auth_page.get_current_url())
-    reg_page.name_field.send_keys("Татьяна")
-    reg_page.last_name_field.send_keys("Фамилия")
-    reg_page.email_or_mobile_phone_field.send_keys("123456789")
-    reg_page.password_field.send_keys("Test123456789")
-    reg_page.password_confirmation_field.send_keys("Test123456789")
+    reg_page.name_field.send_keys("Ксения")
+    reg_page.last_name_field.send_keys("Леснова")
+    reg_page.email_or_mobile_phone_field.send_keys("11111111111")
+    reg_page.password_field.send_keys("Qwert1234")
+    reg_page.password_confirmation_field.send_keys("Qwert1234")
     reg_page.continue_button.click()
     assert reg_page.message_enter_the_phone_in_the_format.get_text() == "Введите телефон в формате +7ХХХХХХХХХХ или" \
                                                                         " +375XXXXXXXXX, или email в формате example@email.ru"
 
-# Тест-кейс N-16.
+# Тест-кейс ТК-015
 # Тестирование аутентификации зарегестрированного пользователя
 def test_authorisation_valid(web_browser):
     page = AuthPage(web_browser)
-    page.phone.send_keys('+79210916809')
-    page.password.send_keys("Test12345678")
+    page.phone.send_keys('+79999999999')
+    page.password.send_keys("Qwert1234")
     page.btn_login.click()
 
     assert 'https://b2c.passport.rt.ru/account_b2c/page?state=' in page.get_current_url() \
